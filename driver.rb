f5c60bfdef35b715db53cbfed6d03ef58bd1680f
@bendouglasweb <frulaa@yahoo.com>
2015-04-19 00:32:27 +0000

    init

--- driver.rb
+++ driver.rb
@@ -1,15 +1,17 @@
 require 'pi_piper'
 include PiPiper
 
-$pm1_1a = PiPiper::Pin.new(:pin => 5, :direction => :out)
-$pm1_1b = PiPiper::Pin.new(:pin => 6, :direction => :out)
-$pm1_2a = PiPiper::Pin.new(:pin => 13, :direction => :out)
-$pm1_2b = PiPiper::Pin.new(:pin => 19, :direction => :out)
+# Motor one outputs
 
+$pm1_1a = PiPiper::Pin.new(:pin => 5, :direction => :out)  # Coil 1A - Grey Wire
+$pm1_1b = PiPiper::Pin.new(:pin => 6, :direction => :out)  # Coil 1B - Green Wire
+$pm1_2a = PiPiper::Pin.new(:pin => 13, :direction => :out) # Coil 2A - Yellow Wire
+$pm1_2b = PiPiper::Pin.new(:pin => 19, :direction => :out) # Coil 2B - Red Wire
+
+#Global coil state (val: 1-4)
 $state = 1
 
-delay = 0.001
-
+#Method to energize specific coils
 def step(w1,w2,w3,w4)
 
 	w1 ? $pm1_1a.on : $pm1_1a.off
@@ -19,16 +21,17 @@ def step(w1,w2,w3,w4)
 
 end
 
+#De-energize all coils
 def release
-
 	$pm1_1a.off
 	$pm1_2a.off
 	$pm1_1b.off
 	$pm1_2b.off
-	$state = 1
-
+#	$state = 1
 end
 
+# Method to move motor to the next set of coils, progressing from
+# whatever coil/state we are in currently. Passes in either a 0 or 1
 # 0 = Reverse Turn
 # 1 = Forward Turn
 def turn(dir)
@@ -61,26 +64,22 @@ def turn(dir)
 
 end
 
+# Step forward a specified number of times with delay
 def forward(delay,turns)
-
 	turns.times do
 		turn 1
 		sleep delay
 	end
 end
 
+# Same as forward but reverse
 def reverse(delay,turns)
-
 	turns.times do
 
 		turn 0
 		sleep delay
-	
 	end
-
 end
 
-# --------- Begin main program ---------------
-	
-
+# --------- Begin ---------------
 
